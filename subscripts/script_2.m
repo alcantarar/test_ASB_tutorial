f7fab7e4d306d8d2d996d40d09e9c8513a81dd20
Stephen Allen <stephen.allen-1@colorado.edu>
2020-07-24 14:30:41 -0600

    corrected file name

--- subscripts/script_2.m
+++ subscripts/script_2.m
@@ -1,7 +1,8 @@
 %% script_2
 
 %read in data
-data = csvread('data/leaf.cvs');
+% addpath(...add appropriate path...)
+data = csvread('data/leaf.csv');
 %plot data
 l = patch(data(:,1), data(:,2), 'green');
 l.LineWidth = 2;
