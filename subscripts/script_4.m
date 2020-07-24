c5f59c7beb843537e4a4ca5fc3b4614413fbdf16
robbiequarter <rj.qrtr@comcast.net>
2020-07-24 14:30:29 -0600

    Switched x-y axes

--- subscripts/script_4.m
+++ subscripts/script_4.m
@@ -3,7 +3,7 @@
 %read in data
 data = csvread('data/ASB.csv');
 %plot data
-plot(data(:,2), data(:,1), 'k-', 'LineWidth',7);
+plot(data(:,1), data(:,2), 'k-', 'LineWidth',7);
 
 %% SOLUTION: Breakout 1
 
